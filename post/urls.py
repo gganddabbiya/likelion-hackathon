02545bc89846e7eccad6122231ef9a697d9e55d7
unhipark <unhi000@naver.com>
2022-08-19 21:01:21 +0900

    post2

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('postcreate/', views.postcreate, name='postcreate'),
     path('freeposts/', views.freeposts, name='freeposts_list'),
-   # path('freedetail/<int:pk>', views.freePostDetail, name='freeposts_detail'),
+    #path('freedetail/<int:pk>', views.freePostDetail, name='freeposts_detail'),
 ]
--- woa_prj/urls.py
+++ woa_prj/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('festivals/', include("festival.urls")),
     path('accounts/', include('allauth.urls')),
     path('exhibitions', include("exhibition.urls")),
-    path('post', include("post.urls")),
+    path('post/', include("post.urls")),
 ]
